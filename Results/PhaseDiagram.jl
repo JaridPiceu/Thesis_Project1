c2d5aa4675b1b04a82d9ab6f0bc1b77c8c7c18c4
Jarid <jarid.piceu@ugent.be>
2025-10-27 15:08:23 +0100

    Added informative plot

--- Results/PhaseDiagram.jl
+++ Results/PhaseDiagram.jl
@@ -6,10 +6,10 @@ using TensorKit
 include("../TensorMaker/TensorFactory.jl")
 
 # PARAMETERS
-niter = 20
-ndimtrunc = 32
-K = 32
-μ0_range = range(-10, 10, length = 100)
+niter = 10
+ndimtrunc = 16
+K = 16
+μ0_range = range(-2, 2, length = 20)
 λ_range = range(0.1, 1, length = 10)
 
 
@@ -30,7 +30,7 @@ function getΦ4ExpValueSquared(K, μ0, λ, h; niter = 15, ndimtrunc = 16)
 end
 
 
-function getSSB_Data(;
+function getPD_Data(;
         K = K, h = 0, niter = niter, ndimtrunc = ndimtrunc,
         μ0_range = μ0_range,
         λ_range = λ_range
@@ -44,6 +44,7 @@ function getSSB_Data(;
     results = DataFrame(μ0 = Float64[], λ = Float64[], value = Float64[])
 
     for μ0 in μ0_range, λ in λ_range
+        println("Julia is using ", Threads.nthreads(), " threads")
         val = getΦ4ExpValueSquared(K, μ0, λ, h; niter = niter, ndimtrunc = ndimtrunc)
         push!(results, (μ0, λ, val))
         println("Computed μ0 = $μ0, λ = $λ → value = $val")
@@ -55,17 +56,17 @@ function getSSB_Data(;
 end
 
 
-function plotSSB_Data(; filepath = joinpath("Results/Data", "PhaseDiagram.csv"))
+function plotPD_Data(; filepath = joinpath("Results/Data", "PhaseDiagram.csv"))
     df = CSV.read(filepath, DataFrame)
 
     # Convert data to grid format for heatmap
     μ0s = sort(unique(df.μ0))
     λs = sort(unique(df.λ))
-    Z = [df.value[(df.μ0 .== μ0) .& (df.λ .== λ)][1] for λ in λs, μ0 in μ0s]
+    ans = [df.value[(df.μ0 .== μ0) .& (df.λ .== λ)][1] for λ in λs, μ0 in μ0s]
 
     plt = heatmap(
-        μ0s, λs, Z,
-        xlabel = "μ₀^2",
+        μ0s, λs, ans,
+        xlabel = "μ₀²",
         ylabel = "λ",
         colorbar_title = "<ϕ²>",
         title = "Phase diagram: ⟨ϕ²⟩ Grid",
@@ -83,5 +84,33 @@ function plotSSB_Data(; filepath = joinpath("Results/Data", "PhaseDiagram.csv"))
     return plt
 end
 
-getSSB_Data()
-plotSSB_Data()
+function plotPD_crosssection(; filepath = joinpath("Results/Data", "PhaseDiagram.csv"), logscale=false)
+    df = CSV.read(filepath, DataFrame)
+
+            # Ensure Data directory exists
+    if !isdir("Results/Plots")
+        mkpath("Results/Plots")
+    end
+
+    # Convert data to grid format for heatmap
+    μ0s = sort(unique(df.μ0))
+    ans = [df.value[(df.μ0 .== μ0) .& (df.λ .== 1)][1] for μ0 in μ0s]
+
+    if logscale
+        plt = scatter(μ0s, ans, yscale=:log10)
+        plotpath = joinpath("Results/Plots", "PD_crossectionLog.png")
+        savefig(plt, plotpath)
+        println("✅ Plot saved to $plotpath") 
+    else
+        plt = scatter(μ0s, ans)
+        plotpath = joinpath("Results/Plots", "PD_crossection.png")
+        savefig(plt, plotpath)
+        println("✅ Plot saved to $plotpath")
+    end
+    return plt
+end
+
+getPD_Data()
+plotPD_Data()
+plotPD_crosssection(logscale=false)
+plotPD_crosssection(logscale=true)
